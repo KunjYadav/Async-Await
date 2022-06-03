c50d0afe4d9e2200f507a32b08d90fd78cf1e7d4
KunjYadav <kunj.yadav555@gmail.com>
2022-06-03 19:38:19 +0530

    second commit

--- promises.js
+++ promises.js
@@ -1,27 +1,46 @@
 console.log('person1: show ticket');
 console.log('person2: show ticket');
 
-const promiseWifeBringingTicks = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('ticket');
-    }, 3000)
-});
+const preMovie = async () => {
 
-const getPopcorn = promiseWifeBringingTicks.then((t) => {
-    console.log('wife: i have the ticks');
+    const promiseWifeBringingTicks = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('ticket'), 3000);
+    });
+
+    const getPopcorn = new Promise((resolve, reject) => resolve(`popcorn`));
+
+    const addButter = new Promise((resolve, reject) => resolve(`butter`));
+
+    const getColdDrinks = new Promise((resolve, reject) => resolve(`coldDrinks`));
+
+    let ticket = await promiseWifeBringingTicks;
+
+    console.log(`wife: i have the ${ticket}`);
     console.log('husband: we should go in');
     console.log('wife: no i am hungry');
-    return new Promise((resolve, reject) => resolve(`${t} popcorn`) );
-});
 
-const getButter = getPopcorn.then((t) => {
-    console.log('husband: i got some popcorn');
+    let popcorn = await getPopcorn;
+
+    console.log(`husband: i got some ${popcorn}`);
     console.log('husband: we should go in');
     console.log('wife: I need butter on my popcorn');
-    return new Promise((resolve, reject) => resolve(`${t} butter`) );
-});
 
-getButter.then((t) => console.log(t));
+    let butter = await addButter;
+
+    console.log(`husband: i got some ${butter} on popcorn`);
+
+
+    let coldDrinks = await getColdDrinks;
+
+    console.log(`wife: i wants ${coldDrinks} too`);
+    console.log(`husband: anything else darling?`);
+    console.log(`wife: lets got we are getting late`);
+    console.log(`husband: thank you for the reminder *grins*`);
+
+    return ticket;
+}
+
+preMovie().then((m) => console.log(`person3: shows ${m}`));
 
 console.log('person4: show ticket');
 console.log('person5: show ticket');
